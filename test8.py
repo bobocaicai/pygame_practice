2b9b8525c82a545d44485343e2d331b30f5fcc0e
bobocaicai <jiangbo__lee@163.com>
2016-12-09 21:25:36 +0800

    画出所有颜色并保存图片

---
+++ test8.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 09 20:53:27 2016
+
+@author: student
+"""
+import pygame
+pygame.init()
+screen=pygame.display.set_mode((640,480))
+all_colors=pygame.Surface((4096,4096),depth=24)
+for r in xrange(256):
+    print r+1,'out of 256'
+    x=(r&15)*256
+    y=(r>>4)*256
+    for g in xrange(256):
+        for b in xrange(256):
+            all_colors.set_at((x+g,y+b),(r,g,b))
+pygame.image.save(all_colors,"allcolors.bmp")
